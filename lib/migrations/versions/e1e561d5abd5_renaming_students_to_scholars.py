261a8cb8bfd7dc16c73023ee61ecab91b06c69bb
John <johnnkonge2020@gmail.com>
2023-12-10 09:02:27 +0300

    added some depandencies and made migrations

---
+++ lib/migrations/versions/e1e561d5abd5_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: e1e561d5abd5
+Revises: 791279dd0760
+Create Date: 2023-12-10 09:02:18.778877
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1e561d5abd5'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
